bf4a72e7e0b4bf270ac06daa81c7f4e364604cb9
Qiu <qiuosier@gmail.com>
2016-07-30 22:54:49 -0400

    Load timeline from Gitlab.

---
+++ songbird/xmlns.py
@@ -0,0 +1,8 @@
+class Gitlab(object):
+    entry = '{http://www.w3.org/2005/Atom}entry'
+    title = '{http://www.w3.org/2005/Atom}title'
+    badge = '{http://search.yahoo.com/mrss/}thumbnail'
+    time = '{http://www.w3.org/2005/Atom}updated'
+    author = '{http://www.w3.org/2005/Atom}author'
+    name = '{http://www.w3.org/2005/Atom}name'
+    email = '{http://www.w3.org/2005/Atom}email'
