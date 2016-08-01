b11162b0a068b7bb71c83ece1674c1e1d4bd2dba
Qiu <qiuosier@gmail.com>
2016-07-31 22:37:20 -0400

    Add security.

--- rhythm/settings.py
+++ rhythm/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
+from rhythm.private import DJANGO_SECRET_KEY
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'esxum4d=-me7u+$aj3x5fk+fq@*f6y_kpkm#6$7r_d^-k%-b-p'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
