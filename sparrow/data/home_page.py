90d05fe5be68f2136a376ef151669d9619c403e4
Qiu <qiuosier@gmail.com>
2017-06-15 22:58:53 -0400

    Add home page.

---
+++ sparrow/data/home_page.py
@@ -0,0 +1,47 @@
+CAROUSEL_DATA = [
+    {
+        'key': 'Downtown Manhattan',
+        'description': 'New York City',
+        'date': 'April, 2015',
+        'image': '/static/images/carousel/IMGP3844.jpg',
+    },
+    {
+        'key': 'Mt. Everest',
+        'description': 'An Unforgettable Bike Trip',
+        'date': 'July, 2011',
+        'image': '/static/images/carousel/MtEverestSunset.jpg',
+    },
+    {
+        'key': 'Xi\'an',
+        'description': 'The Ancient City',
+        'date': 'November, 2008',
+        'image': '/static/images/carousel/Xian.jpg',
+    }
+]
+
+BIRDS = [
+    {
+        'name': 'Sparrow',
+        'description': 'Code',
+        'link': '/sparrow',
+        'image': '/static/images/logo/Sparrow.jpg',
+    },
+    {
+        'name': 'Swift',
+        'description': 'Blog',
+        'link': '/sparrow',
+        'image': '/static/images/logo/Swift.jpg',
+    },
+    {
+        'name': 'Skylark',
+        'description': 'Apreture',
+        'link': '/sparrow',
+        'image': '/static/images/logo/Skylark.jpg',
+    },
+    {
+        'name': 'Swan',
+        'description': 'Journey',
+        'link': '/sparrow',
+        'image': '/static/images/logo/Swan.jpg',
+    }
+]
--- sparrow/views.py
+++ sparrow/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .data.home_page import CAROUSEL_DATA, BIRDS
 
 
 # Create your views here.
 def index(request):
-    return render(request, "sparrow/index.html", {})
+    return render(request, "sparrow/index.html", {
+        'carousel_data': CAROUSEL_DATA,
+        'birds': BIRDS,
+    })
