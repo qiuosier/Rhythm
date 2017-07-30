7c69a0fe28a1b7094895f73c66826cd26be1179f
Qiu <qiuosier@gmail.com>
2017-07-30 13:52:22 -0400

    Add function to show cards.

--- nest/urls.py
+++ nest/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'nest'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^rhythm/(?P<file_name>.*)/$', views.load_data_and_render, name='rhythm'),
-    url(r'^markdown/(?P<file_name>.*)/$', views.page, name='markdown'),
+    url(r'^rhythm/(?P<json_name>.*)/$', views.load_data_and_render, name='rhythm'),
+    url(r'^page/(?P<filename>.*)/$', views.page, name='page'),
+    url(r'^cards/(?P<filename>.*)/$', views.cards, name='cards'),
     url(r'^activate/$', views.activate, name='activate')
 ]
--- nest/views.py
+++ nest/views.py
@@ -13,16 +13,16 @@ def index(request):
     return load_data_and_render(request, "index")
 
 
-def load_json(file_name):
+def load_json(filename):
     """Loads data from a JSON file to a Python dictionary
     JSON files are stored in the "/data" folder.
     Data will be empty if JSON file does not exist.
 
     Args:
-        file_name: Filename without extension.
+        filename: Filename without extension.
 
     """
-    json_file = os.path.join(settings.BASE_DIR, "data", file_name + ".json")
+    json_file = os.path.join(settings.BASE_DIR, "data", filename + ".json")
     if os.path.exists(json_file):
         with open(json_file) as f:
             data = json.load(f)
@@ -32,50 +32,57 @@ def load_json(file_name):
     return data
 
 
-def load_data_and_render(request, file_name):
+def load_data_and_render(request, json_name, html_name=None):
     """Loads data from a JSON file and renders a template.
-    The JSON file and HTML template should have the same filename (different extensions, json and html).
     JSON files are stored in the "/data" folder.
     HTML files are stored in the "nest" template folder.
 
     Args:
         request: HTTP request.
-        file_name: Filename without extension.
+        json_name: JSON filename without extension.
+        html_name: HTML filename without extension.
+        If html_name is not specified, the json_name will be also used as the HTML filename.
 
     Returns: HTTP Response.
 
     """
-    html_file = "nest/" + file_name + ".html"
+    if html_name is None:
+        html_name = json_name
+    html_file = "nest/" + html_name + ".html"
     try:
         get_template(html_file)
     except TemplateDoesNotExist:
         message = "Template \"%s\" does not exist." % html_file
         return HttpResponseNotFound(message)
-    return render(request, html_file, load_json(file_name))
+    return render(request, html_file, load_json(json_name))
 
 
-def page(request, file_name):
+def page(request, filename):
     """Loads data from a markdown file and renders it in the "page" template.
     Markdown files are stored in the "static/data" folder.
 
     Args:
         request: HTTP request.
-        file_name: Filename without extension.
+        filename: Filename without extension.
 
     Returns: HTTP Response.
 
     """
-    input_file = os.path.join(settings.BASE_DIR, "data", "markdown", file_name + ".md")
+    input_file = os.path.join(settings.BASE_DIR, "data", "markdown", filename + ".md")
     if os.path.exists(input_file):
         with open(input_file) as f:
             text = f.read()
     else:
-        return HttpResponseNotFound(file_name + " not found.")
+        return HttpResponseNotFound(filename + " not found.")
     html_content = markdown.markdown(
         text,
         output_format="html5",
     )
     return render(request, "nest/page.html", {"html_content": html_content})
+
+
+def cards(request, filename):
+    return load_data_and_render(request, filename, "cards")
 
 
 def activate(request):
