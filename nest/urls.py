b39b7974a3a1e82d7781299aa6a9cae19ef792ec
Qiu <qiuosier@gmail.com>
2018-01-31 23:28:47 -0500

    Update Swan home page.

--- nest/urls.py
+++ nest/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'nest'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^skylark/(?P<collection>.*)/$', views.skylark_collection, name='skylark_collection'),
     url(r'^rhythm/(?P<json_name>.*)/$', views.load_data_and_render, name='rhythm'),
+    url(r'^skylark/(?P<collection>.*)/$', views.skylark_collection, name='skylark_collection'),
     url(r'^page/(?P<filename>.*)/$', views.page, name='page'),
     url(r'^cards/(?P<filename>.*)/$', views.cards, name='cards'),
     url(r'^timeline/(?P<filename>.*)/$', views.timeline, name='timeline'),
