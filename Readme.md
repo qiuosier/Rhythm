6f6add58ac54bc4aac020db2983f9e5cc23f55ec
Qiu <qiuosier@gmail.com>
2018-05-22 22:17:49 -0400

    Add comments.

--- Readme.md
+++ Readme.md
@@ -22,9 +22,10 @@ Other than the logging middleware, this project does not use external database. 
 ### Rendering Webpage with Data
 The `nest/view.py` module contains two basic view functions for rendering the "JSON" and "Markdown" data, respectively. 
 * The `load_data_and_render` function loads a JSON file from the data folder and render an HTML template in the tempalte folder (most likely with the same filename, e.g. hello.json and hello.html).
-* The `page` function loads a markdown file and render it with the `page.html` template.
+* The `page` function loads a markdown file and render it with the `page.html` template. The URL pattern for displaying a "page" is `www.example.com/nest/page/<file_path>/`, in which `<file_path>` is the file path of a markdown file stored in `/data/markdown/`. Since both the URL and the file path are two level down from the root, relative paths in the markdown file will be able to point to the same location when the file is being viewed by a local editor or rendered by the web application. The images used in the markdown files are stored in `/static/images/`.
 
 ## Updating Pages with Management Commands
 ### Update Swift
+The Swift home page displays blog entries. It loads data from `/data/swift.json`, which should be updated as new blogs are added. The `update_swift` management command is designed to update the `/data/swift.json`. The command loads the blogs, generate summaries and updates the entries to be displayed on the Swift home page.
 
 ### Update Swan
--- nest/management/commands/update_swift.py
+++ nest/management/commands/update_swift.py
@@ -4,23 +4,34 @@ from operator import itemgetter
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-
+# The folder storing the Markdown blog files.
+# The filename should have the format of 20160101_ArticleName.md, i.e. a date and a name separated by '_'
 BLOGS_FOLDER = os.path.join(settings.BASE_DIR, "data", "markdown", "swift")
 
+# The JSON file storing the Swift home page entries
 JSON_OUTPUT = os.path.join(settings.BASE_DIR, "data", "swift.json")
 
+# The size array for the blog entries showing on the home page.
+# Each number indicates the width of the blog entry on the page.
+# This number is the same number as the ones used in the bootstrap column width (e.g., col-md-8).
+# The page has a full width of 12
 SIZE_ARRRY = [8, 4, 4, 4, 4, 6, 6, 4, 4, 4, 6, 6]
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        # Load all file names.
         files = [os.path.join(BLOGS_FOLDER, f) for f in os.listdir(BLOGS_FOLDER) if os.path.isfile(os.path.join(BLOGS_FOLDER, f))]
         entries = []
         for filename in files:
             with open(filename, 'r') as f:
                 lines = f.readlines()
+                # Title of the blog
                 title = None
+                # Date of the blog
                 date = None
+                # The first image
                 image = None
+                # Introduction text
                 intro = None
                 for line in lines:
                     if title and date and image and intro:
@@ -53,15 +64,17 @@ class Command(BaseCommand):
                     "key": os.path.basename(filename).split('_')[0],
                     "class": "col-md-6"
                 })
+        # Sort by date
         entries = sorted(entries, key=itemgetter('key'), reverse=True)
+        # Take only the first 12 entries
         if len(entries) > 12:
             entries = entries[:12]
-        
+        # Set entry width
         i = 0
         for entry in entries:
             entry["class"] = "col-md-" + str(SIZE_ARRRY[i])
             i += 1
-
+        # Save the data
         with open(JSON_OUTPUT, 'w') as output:
             json.dump({
                 "title": "The Swift Blog",
