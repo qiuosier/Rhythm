cb8e2ad65a3a0229122ea1dda53b3635e4e8da63
Qiu <qiuosier@gmail.com>
2018-11-11 12:01:44 -0500

    Add credit card rewards table.

---
+++ data/markdown/swift/20181110_CreditCards.md
@@ -0,0 +1,5 @@
+# Which Credit Card I Should Use to Get Max Rewards?
+
+_November 10th, 2018_
+
+<div class="ajax-page-component" data-context="credit_cards" data-template="credit_cards">Loading data...</div>
--- nest/transform.py
+++ nest/transform.py
@@ -4,17 +4,70 @@ from collections import OrderedDict
 def credit_cards(data):
     cards = data.get("cards", [])
     # Get all the available categories
+    # key = category name
+    # value = a list of tuples (card, normalized reward value) for cards having this reward category.
     categories = {}
     for card in cards:
         reward = card.get("reward", {})
-        reward_cat = reward.get("categories", {})
-        for cat, value in reward_cat.items():
-            categories[cat] = categories.get(cat, 0) + 1
+        reward_categories = reward.get("categories", {})
+        for category, value in reward_categories.items():
+            # Normalize the reward value
+            reward_value = reward.get("value", 1)
+            normalized_value = round(value * reward_value, 1)
+            reward["value"] = round(reward_value, 2)
+            reward_categories[category] = normalized_value
+            # Add card to the category
+            card_list = categories.get(category, [])
+            card_list.append((card, normalized_value))
+            categories[category] = card_list
+    categories = OrderedDict(sorted(categories.items(), key=lambda (k, v): len(v), reverse=True))
     
-    categories = OrderedDict(sorted(categories.items(), key=lambda (k, v): v, reverse=True))
-    print(categories)
+    # Store the categories with only one card
+    singles = []
+    # For each category, store the max and min reward cards.
+    # key = category
+    # value = a list of cards with max or min rewards in this category
+    max_rewards = {}
+    min_rewards = {}
+
+    for category, card_list in categories.items():
+        # Store the categories with only one card
+        if len(card_list) == 1:
+            singles.append(category)
+        # Find the cards with max and min reward in each category
+        reward_list = []
+        for card in cards:
+            reward = card.get("reward", {})
+            reward_categories = reward.get("categories", {})
+            reward_value = reward_categories.get(category, reward_categories.get("Base"))
+            reward_list.append((card, reward_value))
+        reward_list.sort(key=lambda (k, v): v, reverse=True)
+        # The max and min reward values in this category
+        max_value = reward_list[0][1]
+        min_value = reward_list[-1][1]
+        for card, value in reward_list:
+            if value == max_value:
+                l = max_rewards.get(category, [])
+                l.append(card["name"])
+                max_rewards[category] = l
+            if value == min_value:
+                l = min_rewards.get(category, [])
+                l.append(card["name"])
+                min_rewards[category] = l
+
+    # Remove categories that have only one card (Show the rewards in "Others")
+    for category in singles:
+        card = categories[category][0][0]
+        reward = card.get("reward", {})
+        reward_categories = reward.get("categories", {})
+        others = reward_categories.get("Others", [])
+        others.append("%s: %.1f" % (category, reward_categories[category]))
+        reward_categories["Others"] = others
+        categories.pop(category)
     context = {
         "categories": categories,
-        "cards": cards
+        "cards": cards,
+        "max_rewards": max_rewards,
+        "min_rewards": min_rewards,
     }
     return context
