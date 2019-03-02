6ce943028779bf7e396e59dbcd23102a4d952083
Qiu <qiuosier@gmail.com>
2019-03-01 20:01:34 -0500

    Update commands to update thumbnails

--- nest/lib.py
+++ nest/lib.py
@@ -1,6 +1,7 @@
 import json
 import os
 from django.conf import settings
+from PIL import Image
 
 
 def load_json(filename):
@@ -63,3 +64,73 @@ def search_file(search_dirs, filename, root=""):
         if os.path.exists(absolute_path):
             return relative_path
     return None
+
+
+def get_files(dir_path):
+    return [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+
+
+def parse_markdown(filename, base_dir):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        # Title of the blog
+        title = None
+        # Date of the blog
+        date = None
+        # The first image
+        image = None
+        # Introduction text
+        intro = ""
+        for line in lines:
+            if title and date and image and intro:
+                break
+            line = line.replace('\r', '').replace('\n', '')
+            if len(line) == 0:
+                continue
+            if not title and line[0] == '#':
+                title = line[1:].strip(' ')
+                continue
+            if not date and line[0] == '_' and line[-1] == '_':
+                date = line.strip('_')
+                continue
+            if not image and line.startswith('!['):
+                image = line[line.find("/static/"):].strip(')')
+                continue
+            if not intro and line[0].isalpha():
+                if len(line) > 90:
+                    intro = line[:line.find(' ', 90)]
+                else:
+                    intro = line
+                intro = intro + '...'
+                continue
+        return {
+            "title": title,
+            "name": filename.split('.')[0].replace(base_dir, "")[1:],
+            "image": image,
+            "summary": intro,
+            "date": date,
+            "key": os.path.basename(filename).split('_')[0],
+        }
+
+
+def resize_image(image_file, output_file, to_width, to_height):
+    try:
+        im = Image.open(image_file)
+        w, h = im.size
+        # Resize the image
+        # w/h < to_w/to_h means the image is too tall
+        if (w / h) < (to_width / to_height):
+            # Preserve the width if the image is too tall
+            im.thumbnail((to_width, to_width * 10), Image.ANTIALIAS)
+        else:
+            # Preserve the height if the image is too wide
+            im.thumbnail((to_height * 10, to_height), Image.ANTIALIAS)
+        w, h = im.size
+        mid_w = w / 2
+        mid_h = h / 2
+        im = im.crop((mid_w - to_width / 2, mid_h - to_height / 2, mid_w + to_width / 2, mid_h + to_height / 2))
+        im.save(output_file, "JPEG")
+        print("Resized %s" % image_file)
+    except IOError as ex:
+        print(ex)
+        print("Cannot create thumbnail for '%s'" % image_file)
--- nest/management/commands/update_thumbnails.py
+++ nest/management/commands/update_thumbnails.py
@@ -1,64 +1,13 @@
 import os
 import json
-import re
 from operator import itemgetter
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from PIL import Image
+from nest.lib import resize_image, get_files
 
 
-SWAN_FOLDER = os.path.join(settings.BASE_DIR, "data", "markdown", "swan")
-THUMBNAIL_FOLDER = os.path.join(settings.BASE_DIR, "static", "images", "swan", "home")
-IMAGE_SUB_FOLDER = "static/images"
-IMAGE_FOLDER = os.path.join(settings.BASE_DIR, IMAGE_SUB_FOLDER)
 
 SKYLARK_IMAGE_FOLDER = os.path.join(settings.BASE_DIR, "static", "images", "skylark")
-
-
-def get_files(dir_path):
-    return [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
-
-
-def resize_image(image_file, output_file, to_width, to_height):
-    try:
-        im = Image.open(image_file)
-        w, h = im.size
-        # Resize the image
-        # w/h < to_w/to_h means the image is too tall
-        if (w / h) < (to_width / to_height):
-            # Preserve the width if the image is too tall
-            im.thumbnail((to_width, to_width * 10), Image.ANTIALIAS)
-        else:
-            # Preserve the height if the image is too wide
-            im.thumbnail((to_height * 10, to_height), Image.ANTIALIAS)
-        w, h = im.size
-        mid_w = w / 2
-        mid_h = h / 2
-        im = im.crop((mid_w - to_width / 2, mid_h - to_height / 2, mid_w + to_width / 2, mid_h + to_height / 2))
-        im.save(output_file, "JPEG")
-        print("Resized %s" % image_file)
-    except IOError as ex:
-        print(ex)
-        print("Cannot create thumbnail for '%s'" % image_file)
-
-
-def update_swan_home():
-    image_pattern = r"!\[.*\]\(.*/%s/.*jpg\)" % IMAGE_SUB_FOLDER
-    markdown_files = get_files(SWAN_FOLDER)
-    for markdown_file in markdown_files:
-        thumbnail_name = markdown_file.replace(".md", ".jpg")
-        thumbnail_file = os.path.join(THUMBNAIL_FOLDER, thumbnail_name)
-        image_file = None
-        with open(os.path.join(SWAN_FOLDER, markdown_file), 'r') as f:
-            # Find the first image
-            for line in f:
-                if re.match(image_pattern, line):
-                    image_file = line[line.find(IMAGE_SUB_FOLDER):]\
-                        .replace(IMAGE_SUB_FOLDER, IMAGE_FOLDER)\
-                        .split(")")[0]
-                    break
-        if image_file:
-            resize_image(image_file, thumbnail_file, 400, 300)
 
 
 def update_skylark(folder):
