27a95010070c6d902535f5be0c70f09a78002639
Qiu <qiuosier@gmail.com>
2019-05-31 23:00:41 -0400

    Delete log http request middleware.
    Stackdriver logging already have the request logs.

--- rhythm/settings.py
+++ rhythm/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # "nest.middleware.LogHttpRequestMiddleware"
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
