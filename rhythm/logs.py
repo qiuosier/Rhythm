c63106ab6ad634137b6e45ca13bbbce38b459ca4
Qiu <qiuosier@gmail.com>
2019-12-14 12:28:26 -0500

    Update Aries and logs.py

--- rhythm/logs.py
+++ rhythm/logs.py
@@ -1,67 +1,46 @@
 import os
 import sys
 import socket
+from Aries.outputs import LoggingConfigDict
 
 
-LOG_HANDLERS = {
-    'console': {
-        'level': 'DEBUG',
-        'filters': ['require_debug_true', 'project_packages'],
-        'class': 'logging.StreamHandler',
-        'formatter': 'Aries'
-    }
-}
-HANDLER_NAMES = ['console']
 LOGGING_LEVEL = 'DEBUG'
-
+config_dict = LoggingConfigDict().add_logger("", LOGGING_LEVEL)
 
 # Stackdriver logging will NOT be enabled if
 #   There is a file called "DEBUG" in the project root, or
 #   The website is running by using the runserver command.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if not os.path.exists(os.path.join(BASE_DIR, "DEBUG")) and not 'runserver' in sys.argv:
+if True: # not os.path.exists(os.path.join(BASE_DIR, "DEBUG")) and not 'runserver' in sys.argv:
     logger_name = socket.gethostname()
     # Check if the website is running in Google App Engine
-    if logger_name == 'localhost' and os.getenv('GAE_RUNTIME', '') == "python37":
+    if os.getenv('GAE_RUNTIME', '') == "python37":
         logger_name = 'AppEngine'
 
     from google.cloud import logging
     client = logging.Client()
     client.setup_logging()
-    LOG_HANDLERS['stackdriver'] = {
-        'class': 'google.cloud.logging.handlers.CloudLoggingHandler',
+    stackdriver_handler = {
+        'handler_class': 'Aries.gcp.logging.StackdriverHandler',
+        'filters': ['project_packages'],
         'client': client,
-        'name': logger_name
+        'name': logger_name,
+        'handler_name': "stackdriver"
     }
     print("Using Stackdriver logging...")
-    HANDLER_NAMES = ['stackdriver']
-    LOGGING_LEVEL = 'DEBUG'
+    config_dict.add_handler(**stackdriver_handler)
+else:
+    config_dict.add_handler(
+        handler_name='console',
+        handler_class="Aries.outputs.StreamHandler"
+    )
 
 
-RHYTHM_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'Aries': {
-            '()': 'Aries.outputs.MessageFormatter',
-        },
+config_dict.add_filters({
+    'project_packages': {
+        '()': 'Aries.outputs.PackageLogFilter',
+        'package_root': os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     },
-    'filters': {
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-        'project_packages': {
-            '()': 'Aries.outputs.PackageLogFilter',
-            'package_root': os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        },
-    },
-    'handlers': LOG_HANDLERS,
-    'loggers': {
-        '': {
-            'handlers': HANDLER_NAMES,
-            'level': LOGGING_LEVEL,
-            'propagate': True,
-            'name': 'Django:Rhythm'
-        },
-    },
-}
+})
+
+RHYTHM_CONFIG = config_dict.get_config()
